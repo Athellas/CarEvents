ac90e680aa827f03acb50bd2b42cbd72bc6e5a09
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-05-18 19:38:21 +0200

    386

--- CarEvents/Program.cs
+++ CarEvents/Program.cs
@@ -10,6 +10,52 @@ namespace CarEvents
     {
         static void Main(string[] args)
         {
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // NameOfObject.NameOfEvent += new RelatedDelegate(functionToCall);
+
+            // register event handlers.
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+
+            Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded);
+            c1.Exploded += CarExploded;
+
+            Console.WriteLine("***** Speeding up! *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            // Remove CarExploded method from invocation list
+            c1.Exploded -= CarExploded;
+
+            Console.WriteLine("***** Speeding up! *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+
+
+        }
+
+
+
+        public static void CarAboutToBlow(object sender, CarEventArgs e)
+        {
+            Console.WriteLine($"{sender} says: {e.msg}");
+        }
+
+        public static void CarIsAlmostDoomed(object sender, CarEventArgs e)
+        {
+            if (sender is Car)
+            {
+                Car c = (Car)sender;
+                Console.WriteLine("=> Critical Message from {0}: {1}", c.PetName, e.msg);
+            }
+        }
+
+        public static void CarExploded(object sender, CarEventArgs e)
+        {
+            Console.WriteLine(e.msg);
         }
     }
 }
